0a91eb91e24dcc12dc88aed5861a5e6d896a5fbe
itsmejohndoe <me@johndoe.dev>
2022-06-05 00:03:20 -0300

    feat: add implementation for listing_repository.dart.

--- lib/features/listing/data/repositories/listing_repository.dart
+++ lib/features/listing/data/repositories/listing_repository.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
 
+import '../../../../shared/exceptions/network_exception.dart';
+import '../../../../shared/exceptions/server_exception.dart';
 import '../../../../shared/failures/custom_failure.dart';
+import '../../../../shared/failures/network_failure.dart';
+import '../../../../shared/failures/server_failure.dart';
 import '../../domain/entities/picture.dart';
 import '../../domain/repositories/listing_repository_contract.dart';
 import '../datasources/listing_local_data_source.dart';
 import '../datasources/listing_remote_data_source.dart';
+import '../models/picture_model.dart';
 
 class ListingRepository extends ListingRepositoryContract {
   final ListingLocalDataSourceContract _localDataSource;
@@ -13,8 +18,26 @@ class ListingRepository extends ListingRepositoryContract {
   ListingRepository(this._localDataSource, this._remoteDataSource);
 
   @override
-  Future<Either<CustomFailure, List<Picture>>> listPictures() {
-    // TODO: implement listPictures
-    throw UnimplementedError();
+  Future<Either<CustomFailure, List<Picture>>> listPictures() async {
+    List<Picture> fromModelList(List<PictureModel> list) => list.map((e) => e.toEntity()).toList();
+
+    List<PictureModel>? cache;
+    try {
+      cache = await _localDataSource.retrieve();
+      final result = await _remoteDataSource.getPictures();
+      await _localDataSource.store(result);
+
+      return Right(fromModelList(result));
+    } on NetworkException catch (e) {
+      if (cache == null) {
+        return Left(NetworkFailure(e.message));
+      }
+      return Right(fromModelList(cache));
+    } on ServerException catch (e) {
+      if (cache == null) {
+        return Left(ServerFailure(e.message));
+      }
+      return Right(fromModelList(cache));
+    }
   }
 }
