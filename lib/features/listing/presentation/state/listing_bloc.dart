fec652ab5ed58ab200fdc6b00d39324a1a6be1c4
itsmejohndoe <me@johndoe.dev>
2022-06-05 22:39:52 -0300

    feat: added behavior to listing_bloc.dart.

--- lib/features/listing/presentation/state/listing_bloc.dart
+++ lib/features/listing/presentation/state/listing_bloc.dart
@@ -11,5 +11,12 @@ class ListingBloc extends Bloc<ListingEvent, ListingState> {
     on<LoadListing>((event, emit) => _onLoadListing(event, emit));
   }
 
-  void _onLoadListing(LoadListing _, Emitter emitter) {}
+  void _onLoadListing(LoadListing _, Emitter emit) async {
+    emit(state.copyWith(status: ListingStatus.loading));
+    final result = await _getPicturesList();
+    result.fold(
+      (failure) => emit(state.copyWith(status: ListingStatus.error, failure: failure)),
+      (success) => emit(state.copyWith(status: ListingStatus.ready, data: success)),
+    );
+  }
 }
